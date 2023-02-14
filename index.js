75a24ceea63edc98f0691bba6985d539c4ec8dbb
Bhanu-Ganga-DevOPs <gangabhavanikatraparthi@gmail.com>
2023-02-14 09:30:34 +0000

    changed index.js again and again

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hi everyone, Have a great day!");
+  res.send("Hi everyone, Have a great day!!");
 });
 
 app.listen(8080, () => {
